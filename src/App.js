2990a92d1e726c3f03874eccebbcca8e7587a3ce
Vilmar Cabanero <vilmarcabanero@gmail.com>
2021-05-22 15:29:24 +0800

    Under maintainance

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css'
+import Particles from './components/Particles'
+import UnderDevelopment from './components/UnderDevelopment'
+
+function App() {
+	return (
+		<div>
+			<Particles />
+			<UnderDevelopment />
+		</div>
+	)
+}
+
+export default App
